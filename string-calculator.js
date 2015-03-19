a7bc293aa19f7980f5c718077517bbbbb2cf519a
m4w4q7 <m4w4q7@gmail.com>
2015-03-19 12:42:22 +0000

    v1.6.1 refactoring
    - Removed a nasty, little duplication in the RegExps.
    - Replaced one-line functions with variables.

--- string-calculator.js
+++ string-calculator.js
@@ -1,42 +1,21 @@
-module.exports = (function() {
-	
-	var _limit = 1000;
-	
-	var _getDelimiter = function(calculatorInputString) {
-		var delimiterRegExp = /^\/\/(.)\n/;
-		return delimiterRegExp.test(calculatorInputString) ? delimiterRegExp.exec(calculatorInputString)[1] : /[,\n]/;
-	};
-	
-	var _getNumberSequence = function(calculatorInputString) {
-		var numberSequenceRegExp = /^(?:\/\/.\n)?((?:.|[\n\r])*)$/;
-		return numberSequenceRegExp.exec(calculatorInputString)[1];
-	};
-	
-	var _removeNumbersBeyondTheLimit = function(numbers) {
-		return numbers.filter(function(number) {return number <= _limit});
-	};
-	
-	var sum = function(numbers) {
-		return numbers.reduce( function(e1, e2) {return e1 + e2}, 0 );
-	}
-	
-	var add = function(calculatorInputString) {
+var _limit = 1000;
+
+module.exports.add = function(calculatorInputString) {
 		
-		var delimiter = _getDelimiter(calculatorInputString);
-		var numberSequence = _getNumberSequence(calculatorInputString);
-		
-		if (numberSequence === "") return 0;
-		
-		var numbers = numberSequence.split(delimiter).map( function(s) {return parseInt(s, 10)} );
-		
-		var negativeNumbers = numbers.filter( function(number) {return number < 0} );
-		
-		if (negativeNumbers.length) throw new RangeError("Negatives not allowed: " + negativeNumbers.join());
-		
-		return sum(_removeNumbersBeyondTheLimit(numbers));
-	};
+	var delimiterRegExp = /^(?:\/\/(.)\n)?/g;
 	
-	return {
-		add: add
-	};
-})();
+	var delimiterSequence = delimiterRegExp.exec(calculatorInputString)[1];
+	var numberSequence = calculatorInputString.substr(delimiterRegExp.lastIndex);
+	
+	if (numberSequence === "") return 0;
+	
+	var delimiter = delimiterSequence || /[,\n]/;
+	var numbers = numberSequence.split(delimiter).map( function(s) {return parseInt(s, 10)} );
+	
+	var negativeNumbers = numbers.filter( function(number) {return number < 0} );
+	if (negativeNumbers.length) throw new RangeError("Negatives not allowed: " + negativeNumbers.join());
+	
+	var numbersBeyondTheLimit = numbers.filter(function(number) {return number <= _limit});
+	var sum = numbersBeyondTheLimit.reduce( function(e1, e2) {return e1 + e2}, 0 );
+	return sum;
+};
